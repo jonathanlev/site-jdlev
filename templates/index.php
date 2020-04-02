ae9c23e0116743169f8cb6bfbe6d3e2d0eee315a
Jonathan Leventhal <jonathanleventhal@Jonathans-MacBook-Pro.local>
2020-04-01 22:22:39 -0400

    phpified v.5

--- templates/index.php
+++ templates/index.php
@@ -9,8 +9,8 @@
     <!-- JS -->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
-<?php
-echo '<script>
+
+  <script>
        $(document).ready(function () {
           $(document).on("scroll", onScroll);
 
@@ -48,9 +48,7 @@ echo '<script>
             }
           });
         }
-    </script>'
-    ;
-    ?>
+    </script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
